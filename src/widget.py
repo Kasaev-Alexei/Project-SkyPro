d3bff3b675205badda08fa4dbf4770979d101de8
Alexei Kasaev <kasaev_97@mail.ru>
2023-10-26 23:31:09 +0300

    fix code 3

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,7 @@
 from src.masks import mask_user_account, mask_user_card
 
+check_name_account = ["Maestro", "MasterCard", "Visa Classic", "Visa Platinum", "Visa Gold"]
+
 
 def chek_card_and_account(info: str) -> str:
     """
@@ -8,14 +10,14 @@ def chek_card_and_account(info: str) -> str:
     :return: название карты/счета и маску карты/счета
     """
     list_of_info = info.split()
-    payment_type = list_of_info[0]
-    payment_number = list_of_info[1]
+    join_mask = " ".join(list_of_info[:-1])
 
-    if payment_type == "Maestro" and len(payment_number) == 16:
-        return f"{payment_type} {mask_user_card(payment_number)}"
-    elif payment_type == "Счет" and len(payment_number) == 20:
-        return f"{payment_type} {mask_user_account(payment_number)}"
-    return f"Не верно введен номер карты или название карты"
+    if len(mask_user_account(list_of_info[1])) == 20 or "счет" == list_of_info[0].lower():
+        return f"{join_mask} {mask_user_account(list_of_info[1])}"
+    elif len(list_of_info[-1]) == 16 and join_mask in check_name_account:
+        return f"{join_mask} {mask_user_card(list_of_info[-1])}"
+    else:
+        return f"Не верно введен номер карты/счета или название карты/счета"
 
 
 def date_and_time(date: str) -> str:
@@ -27,6 +29,6 @@ def date_and_time(date: str) -> str:
     return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
 
 
-print(chek_card_and_account("Maestro 1234567890123456"))
+print(chek_card_and_account("Visa Platinum 1234567890123456"))
 print(chek_card_and_account("Счет 12345678901234567890"))
 print(date_and_time("2018-07-11T02:26:18.671407"))
