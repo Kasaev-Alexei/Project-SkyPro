518ce9ea04d2902d3e620c2ecf37909534229698
Alexei Kasaev <kasaev_97@mail.ru>
2023-10-26 11:01:47 +0300

    fix code 2

--- src/widget.py
+++ src/widget.py
@@ -1,36 +1,21 @@
 from src.masks import mask_user_account, mask_user_card
 
 
-def card_name_and_number(name_card: str) -> str:
+def chek_card_and_account(info: str) -> str:
     """
-    Функция принимает на вход номер карты и название карты и проверяет их
-    :param name_card: Номер для маскирования карты и название карты
-    :return: название карты и маску карты
+    Функция принимает на вход номер карты/счета и название карты/счета и проверяет их
+    :param name_card: Номер для маскирования карты/счета и название карты/счета
+    :return: название карты/счета и маску карты/счета
     """
-    name_number_card = name_card.split()
-    check_name_card = name_number_card[0]
-    check_number_card = name_number_card[1]
+    list_of_info = info.split()
+    payment_type = list_of_info[0]
+    payment_number = list_of_info[1]
 
-    if check_name_card == "Maestro" and len(check_number_card) == 16:
-        return f"{check_name_card} {mask_user_card(check_number_card)}"
-    else:
-        return f"Не верно введен номер карты или название карты"
-
-
-def account_name_and_number(name_account: str) -> str:
-    """
-    Функция принимает на вход номер счета и название счета и проверяет их
-    :param name_account: Номер для маскирования счета и название счета
-    :return: название счета и маску счета
-    """
-    name_number_account = name_account.split()
-    check_name_account = name_number_account[0]
-    check_number_account = name_number_account[1]
-
-    if check_name_account == "Счет" and len(check_number_account) == 20:
-        return f"{check_name_account} {mask_user_account(check_number_account)}"
-    else:
-        return f"Не верно введен номер карты или название карты"
+    if payment_type == "Maestro" and len(payment_number) == 16:
+        return f"{payment_type} {mask_user_card(payment_number)}"
+    elif payment_type == "Счет" and len(payment_number) == 20:
+        return f"{payment_type} {mask_user_account(payment_number)}"
+    return f"Не верно введен номер карты или название карты"
 
 
 def date_and_time(date: str) -> str:
@@ -42,6 +27,6 @@ def date_and_time(date: str) -> str:
     return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
 
 
-print(card_name_and_number("Maestro 1234567890123456"))
-print(account_name_and_number("Счет 12345678901234567890"))
+print(chek_card_and_account("Maestro 1234567890123456"))
+print(chek_card_and_account("Счет 12345678901234567890"))
 print(date_and_time("2018-07-11T02:26:18.671407"))
