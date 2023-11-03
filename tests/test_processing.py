bc52883d0055fa0c8d463af02bc3a2e6e23e2f3f
Alexei Kasaev <kasaev_97@mail.ru>
2023-11-04 01:41:08 +0300

    fix name test

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -4,7 +4,7 @@ from src.processing import get_sorted_data, sorted_date
 
 
 @pytest.fixture()
-def test_1() -> list[dict]:
+def test_data() -> list[dict]:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -13,22 +13,22 @@ def test_1() -> list[dict]:
     ]
 
 
-def test_test_1(test_1: list[dict]) -> None:
-    assert get_sorted_data(test_1) == [
+def test_test_1(test_data: list[dict]) -> None:
+    assert get_sorted_data(test_data) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
 
-def test_test_2(test_1: list[dict]) -> None:
-    assert get_sorted_data(test_1, "CANCELED") == [
+def test_test_2(test_data: list[dict]) -> None:
+    assert get_sorted_data(test_data, "CANCELED") == [
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
 
-def test_test_3(test_1: list[dict]) -> None:
-    assert sorted_date(test_1) == [
+def test_test_3(test_data: list[dict]) -> None:
+    assert sorted_date(test_data) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -4,16 +4,16 @@ from src.widget import chek_card_and_account, date_and_time
 
 
 @pytest.mark.parametrize(
-    "x, y",
+    "data, expected",
     [
         ("Visa Platinum 1234567890123456", "Visa Platinum 1234 56** **** 3456"),
         ("Счет 12345678901234567890", "Счет **7890"),
     ],
 )
-def test_parametrize_card_account(x: str, y: str) -> None:
-    assert chek_card_and_account(x) == y
+def test_parametrize_card_account(data: str, expected: str) -> None:
+    assert chek_card_and_account(data) == expected
 
 
-@pytest.mark.parametrize("z, v", [("2018-07-11T02:26:18.671407", "11.07.2018")])
-def test_date_time(z: str, v: str) -> None:
-    assert date_and_time(z) == v
+@pytest.mark.parametrize("data, expected", [("2018-07-11T02:26:18.671407", "11.07.2018")])
+def test_date_time(data: str, expected: str) -> None:
+    assert date_and_time(data) == expected
