e6997d318410959365a8c513eabd4d3e5c0ce65a
Alexei Kasaev <kasaev_97@mail.ru>
2023-11-22 16:38:55 +0300

    add logger for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 import json
 from typing import Any
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler = logging.FileHandler("utils.log", encoding="utf-8")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def get_read_json(the_path_to_the_file: str) -> Any:
@@ -11,9 +20,11 @@ def get_read_json(the_path_to_the_file: str) -> Any:
     try:
         with open(the_path_to_the_file, encoding="utf8") as file:
             bank_transactions = json.load(file)
-    except FileNotFoundError:
+    except FileNotFoundError as error:
+        logger.error(f"Файл {the_path_to_the_file} не найден: {error}")
         bank_transactions = []
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as error:
+        logger.error(f"Ошибка при декодировании JSON в файле{the_path_to_the_file}: {error}")
         bank_transactions = []
     return bank_transactions
 
@@ -25,6 +36,8 @@ def transactions(transaction_data: dict) -> float | str:
     :return: float | str
     """
     if transaction_data["operationAmount"]["currency"]["code"] == "RUB":
+        logger.info(f"вернул сумму транзакции в рублях {transaction_data['operationAmount']['amount']}")
         return float(transaction_data["operationAmount"]["amount"])
     else:
+        logger.error("Транзакция выполнена не в рублях. Укажите транзакцию в рублях")
         raise ValueError("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
