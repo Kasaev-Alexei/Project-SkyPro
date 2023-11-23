21badad60b4dae949610404e35882da8415658ab
Alexei Kasaev <kasaev_97@mail.ru>
2023-11-23 19:56:47 +0300

    moved the log file to a separate file

---
+++ src/log.py
@@ -0,0 +1,8 @@
+import logging
+
+logger = logging.getLogger('__masks__')
+file_handler_masks = logging.FileHandler('my_log.log', 'w', encoding='utf-8')
+file_formatter_masks = logging.Formatter('%(asctime)s %(module)s %(levelname)s %(message)s')
+file_handler_masks.setFormatter(file_formatter_masks)
+logger.addHandler(file_handler_masks)
+logger.setLevel(logging.INFO)
